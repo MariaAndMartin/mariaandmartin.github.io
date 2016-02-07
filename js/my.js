768e99c5c99682fc0e708aa94aafbacb937e2b3d
Maria Cerase <maria.cerase@gmail.com>
2016-02-07 14:08:28 +0000

    started off with some i18n

--- js/my.js
+++ js/my.js
@@ -1,44 +1,57 @@
-
-
-/*$('#rsvp').one('submit',function(){
-    var response = "entry.69463734=" + encodeURIComponent($('[name=group1]').val());
-    var dietary = "entry.2034397929=" + encodeURIComponent($('#dietary').val());
-    var baseURL = 'https://docs.google.com/forms/d/1Nc3aCH5E5p-_17PmCJxURf-Rks5X0QxsHY_o4ElHdNM/formResponse?';
-    var submitRef = '&submit=Submit';
-    var submitURL = (baseURL + response +"&" + dietary + "&" + submitRef);
-    console.log(submitURL);
-    $(this)[0].action=submitURL;});*/
-
-function submitForm(form){
-    var url = "https://docs.google.com/forms/d/1Nc3aCH5E5p-_17PmCJxURf-Rks5X0QxsHY_o4ElHdNM/formResponse?";
-    var payload = new Object();
-    for (i=0; i < form.elements.length -1; i++){
-        if (form[i].type =="radio"){
-            //process the radio button values
-            if (form[i].checked == true){
-                // we found the selected radio button
-                var entryId = form[i].name;
-                var value = encodeURI(form[i].id);
-                payload[entryId] = value;
-            }
-        } else if (form[i].type == "textarea"){
-            //process the text area
-            var entryId = form[i].name;
-            var value = encodeURI(form[i].value);
-            payload[entryId] = value;
-        }
-    }
-    // create httprequest object
-    var req = new XMLHttpRequest();
-    var params = "";
-    // create url for the request
-    for (var k in payload){
-        var params = params.concat(String(k),"=",String(payload[k]),"&");
-    }
-    var submitURL = url.concat(params,"submit=Submit");
-    console.log(submitURL)
-    req.open("GET",submitURL,true);
-    req.setRequestHeader("Access-Control-Allow-Origin","*")
-    req.send();
+function detectLang(){
+	var userLang = navigator.language || navigator.userLanguage; 
+ 	return userLang;
 }
 
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'content.json', true); 
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 	
+function getAllElements(){
+	var allElements = document.getElementsByTagName("*");
+	var allIds = [];
+	for (var i = 0, n = allElements.length; i < n; ++i) {
+  		var el = allElements[i];
+  		if (el.id) { allIds.push(el.id); }
+	}
+	return allIds
+}
+function translate(){
+	loadJSON(function(response) {
+  		// Parse JSON string into object
+    	var contentObject = JSON.parse(response);
+    	// Get language
+    	var lang = detectLang();
+    	//find all elements
+    	var allElementsId = getAllElements();
+    	//translate page
+    	if (lang.indexOf("fr")){
+    		//translate in french
+    		for (i=0 ; i > allElementsId.length; i++){
+    			var el = document.getElementById(allElementsId[i]);
+    			//check if this element is in the content json details
+    			var frContent = contentObject[]
+    		}
+
+    	} else {
+    		//translate in italian
+    	}
+
+
+ 	});
+
+
+}
+
+
+translate();
