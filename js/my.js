09492b516300cd0241094e65ba401f9f5a134f28
martin <m.gruau@gmail.com>
2016-02-07 16:36:43 +0000

    new version

--- js/my.js
+++ js/my.js
@@ -36,13 +36,16 @@ function translate() {
 	//translate page
 	if (lang.indexOf("fr") > -1) {
 		//translate in french
+        console.log("launches translatein french");
         translateIn("fr-FR", allElementsId);
 
 	} else if (lang.indexOf("it") > -1) {
 		//translate in italian
+        console.log("launches translatein italian");
         translateIn("it-IT", allElementsId);
 	} else {
         // translate in english
+        console.log("launches translatein english");
         translateIn("en-GB", allElementsId);
     }
 }
@@ -50,19 +53,35 @@ function translate() {
 function translateIn(locale, allElementsId) {
     // Parse JSON string into object
 
-    var dataJson = $.getJSON("content.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
+    $.getJSON("http://mariaandmartin.github.io/js/content.json", function(json) {
+        console.log(json);
+
+        var contentObject = JSON.parse(JSON.stringify(json));
+
+        localeObject = contentObject[locale];
+
+        console.log(localeObject);
+
+        for (i=0 ; i > allElementsId.length; i++) { // loops through the IDs of the page
+            
+            
+
+            for (j=0; j > localeObject.length; j++) { // loops through the JSON pieces
+
+                if(allElementsId[i] = localeObject[j]) { // if the ID is found within the JSON
+                    var el = document.getElementById(allElementsId[i]);
+                    el.innerHTML = localeObject[j][0];
+                }
+            }
+            
+
+            // check if this element is in the content json details
+
+       }
     });
-    var contentObject = JSON.parse(dataJson);
-
-    console.log(contentObject);
-    for (i=0 ; i > allElementsId.length; i++) {
-
-        var el = document.getElementById(allElementsId[i]);
-        //check if this element is in the content json details
-        //var frContent = contentObject[];    
-
-    }
 }
 
-translate();
+$(document).ready(function () {
+    console.log("launches translate");
+    translate();
+});
