6cec0fe9cad7ef628dde0f6e9c52b5f57bc60d56
martin <m.gruau@gmail.com>
2016-02-13 13:05:07 +0000

    locale cache

--- js/init.js
+++ js/init.js
@@ -3,10 +3,26 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    console.log(document.getElementById("ENoption"));
-    document.getElementById("ENoption").addEventListener("click", function () {translateIn('en-GB');},false);
-    document.getElementById("FRoption").addEventListener("click", function () {translateIn('it-IT');},false);
-    document.getElementById("IToption").addEventListener("click", function () {translateIn('fr-FR');},false);
-    translate();
+
+
+    // if there is local storage activated: add event listeners. Else: dont display the dropdown.
+
+    if(typeof(Storage) !== "undefined") {
+    	// Code for localStorage/sessionStorage.
+    	document.getElementById("ENoption").addEventListener("click", function () {addtoStorage('en-GB'); translateIn('en-GB');},false);
+    	document.getElementById("IToption").addEventListener("click", function () {addtoStorage('it-IT'); translateIn('it-IT');},false);
+    	document.getElementById("FRoption").addEventListener("click", function () {addtoStorage('fr-FR'); translateIn('fr-FR');},false);
+	} else {
+	    // no local storage = no dropdown displayed.
+	    $('#dropdown1').hide();
+	}
+
+	if (!localStorage.locale || typeof(Storage) == "undefined") {
+		translate();
+	} else {     // else: translatein the locale in local storage
+
+		translateIn(localStorage.locale);
+	}
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space	
